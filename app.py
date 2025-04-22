dc3e43c01accc5bbe9e30820aafcccce779ce089
Daniel Hernandez <DanielAcostaMx1@gmail.com>
2025-04-22 16:18:44 -0600

    added boxplot

--- app.py
+++ app.py
@@ -11,21 +11,17 @@ hist_button = st.button('Construir histograma')
 if hist_button:
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
-
     fig = px.histogram(car_data, x="odometer")
-
     st.plotly_chart(fig, use_container_width=True)
 
 scatter_button = st.button('Construir gráfico de dispersión')
 
 if scatter_button:
     st.write('Creación de un gráfico de dispersión para odómetro vs precio')
-
     fig = px.scatter(car_data, x="odometer", y="price")
-
     st.plotly_chart(fig, use_container_width=True)
 
-    boxplot_button = st.button('Construir boxplot')
+boxplot_button = st.button('Construir boxplot')
 
 if boxplot_button:
     st.write('Creación de una boxplot para comparar precio vs condición del vehículo')
