b1c0961a59a43f9e76e4bc78f14f985b1be034ff
Daniel Hernandez <DanielAcostaMx1@gmail.com>
2025-04-22 16:34:45 -0600

    replaced buttons with checkboxes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.header("Análisis de datos de venta de vehículos")
 
 show_hist = st.checkbox('Mostrar histograma')
 
-if hist_button:
+if show_hist:
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
     fig = px.histogram(car_data, x="odometer")
@@ -16,14 +16,14 @@ if hist_button:
 
 show_scatter = st.checkbox('Mostrar gráfico de dispersión')
 
-if scatter_button:
+if show_scatter:
     st.write('Creación de un gráfico de dispersión para odómetro vs precio')
     fig = px.scatter(car_data, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
 
 show_boxplot = st.checkbox('Mostrar boxplot')
 
-if boxplot_button:
+if show_boxplot:
     st.write('Creación de una boxplot para comparar precio vs condición del vehículo')
     fig_box = px.box(car_data, x="condition", y="price",
                      title="Precio por condición del vehículo")
