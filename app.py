cdf2edf921b2343afebf0ded3e32aa9530f6b071
Daniel Hernandez <DanielAcostaMx1@gmail.com>
2025-04-22 16:03:20 -0600

    added notebook and app.py

--- app.py
+++ app.py
@@ -1,4 +1,25 @@
+import pandas as pd
+import plotly.express as px
 import streamlit as st
 
-st.title("¡Hola, Streamlit!")
-st.write("Si ves esto, todo está funcionando 😄")
+car_data = pd.read_csv('vehicles_us.csv') 
+
+st.header("Análisis de datos de venta de vehículos")
+
+hist_button = st.button('Construir histograma')
+
+if hist_button:
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+    
+    fig = px.histogram(car_data, x="odometer")
+    
+    st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir gráfico de dispersión')
+
+if scatter_button:
+    st.write('Creación de un gráfico de dispersión para odómetro vs precio')
+    
+    fig = px.scatter(car_data, x="odometer", y="price")
+    
+    st.plotly_chart(fig, use_container_width=True)
---
+++ test.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title("¡Hola, Streamlit!")
+st.write("Si ves esto, todo está funcionando 😄")
